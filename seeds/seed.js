1f666ccf9c08e5387a44b9d5de43cf94bba94096
saadyousafi87 <saad.yousafi87@gmail.com>
2022-12-14 20:58:25 -0500

    added missing dependencies in package.json file

---
+++ seeds/seed.js
@@ -0,0 +1,23 @@
+const sequelize = require('../config/connection');
+const {Animal, Category} = require('../models');
+
+const categoryData = require('./categoryData.json');
+const animalData = require('./animalData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({force: true});
+
+    const category = await Category.bulkCreate(categoryData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    const animal = await Animal.buldCreate(animalData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    process.exit(0);
+};
+
+seedDatabase();
