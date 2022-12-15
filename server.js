8c47b104781df71ae92eded1fac6999bcfdbd6f0
saadyousafi87 <saad.yousafi87@gmail.com>
2022-12-14 21:20:56 -0500

    sending multiple files

--- server.js
+++ server.js
@@ -2,21 +2,50 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
+const session = require('express-session');
+const routes = require('./controllers');
+const helpers = reqire('./utils/helpers');
+
+const sequelize = require('./config/connection');
+const SeqelizeStore = require('connect-session-sequelize')(session.Store);
+
 
 // Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 4001;
 
-app.engine('handlebars', exphbs.engine());
-app.set('view engine', 'handlebars');
-app.set('views', './views');
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
 
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {
+      maxAge: 300000,
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+//app.set('views', './views');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Sets up the routes
-app.use(require('./controllers/pets-routes'));
+app.use(routes);
 
 //Starts the server to begin listening
-app.listen(PORT, () => {
-    console.log('server listening on: http://localhost:' + PORT)
+sequelize.sync({force: false}).then (() => {
+    app.listen(PORT, () => console.log('new listening at http://localhost:'+PORT));
 });
