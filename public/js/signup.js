efdf92c15fd6e50526179f8df98a3b7840613812
saadyousafi87 <saad.yousafi87@gmail.com>
2022-12-18 14:14:35 -0500

    added comment and updated ids in query selector

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,11 +1,13 @@
+
+// signup event handler to send form data by post method
 const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const first_name = document.querySelector('#first-name').value;
-    const last_name = document.querySelector('#last-name').value;
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value;
-    const phone = document.querySelector('#telephone').value;
+    const first_name = document.querySelector('#inputFirstName').value;
+    const last_name = document.querySelector('#inputLastName').value;
+    const email = document.querySelector('#inputEmail').value.trim();
+    const password = document.querySelector('#inputPassword').value;
+    const phone = document.querySelector('#inputTelephone').value;
   
   
     if (first_name && last_name && email && password && phone) {
